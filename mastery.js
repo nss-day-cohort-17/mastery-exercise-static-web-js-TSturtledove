d6597cae9829e125529e61bac3b661201e143cf5
TSturtledove <roberttortorelli@yahoo.com>
2016-12-02 10:30:35 -0600

    added submit button and bega work on event listeners

--- mastery.js
+++ mastery.js
@@ -1,2 +1,5 @@
+
+document.getElementsByClassName("HeyListen").document.addEventListener("click" || "keyUp")
+
 var tall = document.getElementsByClassName("treeHeight")
 var brick = document.getElementsByClassName("treeBuild")
