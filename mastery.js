5de1eabe668735b5d8eacaab3d25c45392842fb7
TSturtledove <roberttortorelli@yahoo.com>
2016-12-02 12:35:19 -0600

    added getElements and event listener to the submit button

--- mastery.js
+++ mastery.js
@@ -7,15 +7,27 @@
 // document.addEventListener("click" || (
 //   .document.addEventListener("keyup", function(e){
 //   if (e.code === "Enter"){}})), function() {
-// document.getElementsByClassName("HeyListen").addEventListener("click", function(){
 
 
+//
+// var tall = document.getElementsByClassName("treeHeight")
+// var brick = document.getElementsByClassName("treeBuild")
+//
+// var change = document.getElementsByClassName("HeyListen")
+//
+//
+// function ta(){
+//
+//
+//
+// // tall.trim()
+// // brick.trim()
+// console.log(tall)
+// console.log(brick)
+//  }
+ var get=document.getElementsByClassName("HeyListen")[0]
 
-var tall = document.getElementsByClassName("treeHeight")
-var brick = document.getElementsByClassName("treeBuild")
-
-tall.trim()
-brick.trim()
-console.log(tall)
-console.log(brick)
- // })
+ get.addEventListener("click", hear, false)
+  function hear(){
+   alert("I can hear you")
+ }
