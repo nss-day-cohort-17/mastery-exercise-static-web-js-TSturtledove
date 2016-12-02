572a9f17b643026264291d4c65ee3397e807ca0f
TSturtledove <roberttortorelli@yahoo.com>
2016-12-02 13:50:02 -0600

    working on the tree building function

--- mastery.js
+++ mastery.js
@@ -11,20 +11,37 @@
 
    var tall = document.getElementsByClassName("treeHeight")[0].value
    var brick = document.getElementsByClassName("treeBuild")[0].value
+  //
+  // console.log(tall.trim())
+  // console.log(brick.trim())
+    tall=tall.trim()
+    brick=brick.trim()
+    console.log(tall)
+    console.log(brick)
 
-  console.log(tall.trim())
-  console.log(brick.trim())
-}
 
-function slimDown(){
-  tall=tall.trim()
-  brick=brick.trim()
-}
 
+
+    if(tall!==NaN && brick.length === 1){
+      // tall=parseInt(tall,10)
+      // tall=tall.value
+      for(r=1; r<=tall; r++){
+        // var onespace = " "
+        console.log((" ")*(tall-r)+brick)
+      }
+    } else {
+      alert("Sorry, it seems you've filled out the boxes wrong.  Try again.")
+    }
+
+
+  }
+
+
+document.addEventListener("keyup", function (e) {
+  if(e.code=="Enter"){
+    // console.log("I hear you")
+
+    // return hear
+  }
+})
  get.addEventListener("click", hear, false)
-
- document.addEventListener("keyup", function (e) {
-   if(e.code==="Enter"){
-     return hear
-   }
- })
