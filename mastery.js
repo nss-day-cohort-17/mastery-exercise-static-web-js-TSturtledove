770812fda763ebd79adcb7e2abbf3dfb44bf599f
TSturtledove <roberttortorelli@yahoo.com>
2016-12-02 10:45:01 -0600

    added more to eventlistener/elementgetter

--- mastery.js
+++ mastery.js
@@ -1,5 +1,8 @@
 
-document.getElementsByClassName("HeyListen").document.addEventListener("click" || "keyUp")
+document.getElementsByClassName("HeyListen").document.addEventListener("click" || (.document.addEventListener("keyup", function(e){
+  if (e.code === "Enter"){}})), function() {
+
 
 var tall = document.getElementsByClassName("treeHeight")
 var brick = document.getElementsByClassName("treeBuild")
+})
