0b37ea19831c50574eef6df1735adf497a5141fb
TSturtledove <roberttortorelli@yahoo.com>
2016-12-02 13:19:33 -0600

    added trimming to entered values

--- mastery.js
+++ mastery.js
@@ -1,38 +1,29 @@
-//
-// document.getElementsByClassName("HeyListen")
-// .addEventListener("click" || (
-//   .document.addEventListener("keyup", function(e){
-//   if (e.code === "Enter"){}})), function() {
-
-// document.addEventListener("click" || (
-//   .document.addEventListener("keyup", function(e){
-//   if (e.code === "Enter"){}})), function() {
 
 
-//
-// var tall = document.getElementsByClassName("treeHeight")
-// var brick = document.getElementsByClassName("treeBuild")
-//
-// var change = document.getElementsByClassName("HeyListen")
-//
-//
-// function ta(){
-//
-//
-//
-// // tall.trim()
-// // brick.trim()
-// console.log(tall)
-// console.log(brick)
-//  }
+// tall.trim()
+// brick.trim()
+
+
  var get=document.getElementsByClassName("HeyListen")[0]
+
  function hear(){
-  alert("I can hear you")
+  // alert("I can hear you")
+
+   var tall = document.getElementsByClassName("treeHeight")[0].value
+   var brick = document.getElementsByClassName("treeBuild")[0].value
+
+  console.log(tall.trim())
+  console.log(brick.trim())
+}
+
+function slimDown(){
+  tall=tall.trim()
+  brick=brick.trim()
 }
 
  get.addEventListener("click", hear, false)
 
- document.addEventListener("keyup",function (e){
+ document.addEventListener("keyup", function (e) {
    if(e.code==="Enter"){
      return hear
    }
