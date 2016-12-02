dac0e14f5b1fada0d53a19c3bcc2e178582b1af2
TSturtledove <roberttortorelli@yahoo.com>
2016-12-02 11:09:57 -0600

    added js tag in HTML

--- mastery.js
+++ mastery.js
@@ -1,8 +1,21 @@
+//
+// document.getElementsByClassName("HeyListen")
+// .addEventListener("click" || (
+//   .document.addEventListener("keyup", function(e){
+//   if (e.code === "Enter"){}})), function() {
 
-document.getElementsByClassName("HeyListen").document.addEventListener("click" || (.document.addEventListener("keyup", function(e){
-  if (e.code === "Enter"){}})), function() {
+// document.addEventListener("click" || (
+//   .document.addEventListener("keyup", function(e){
+//   if (e.code === "Enter"){}})), function() {
+// document.getElementsByClassName("HeyListen").addEventListener("click", function(){
+
 
 
 var tall = document.getElementsByClassName("treeHeight")
 var brick = document.getElementsByClassName("treeBuild")
-})
+
+tall.trim()
+brick.trim()
+console.log(tall)
+console.log(brick)
+ // })
